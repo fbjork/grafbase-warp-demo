4e8dd074e5a5625d90a84550489d7d514dd3d2ed
Fredrik Björk <fredrik.bjork@gmail.com>
2024-02-02 16:37:54 +0100

    initial commit

---
+++ grafbase.config.ts
@@ -0,0 +1,18 @@
+import { graph, connector, config } from "@grafbase/sdk";
+
+const g = graph.Standalone();
+
+const vendure = connector.GraphQL("Vendure", {
+  url: "https://demo.vendure.io/shop-api",
+});
+
+g.datasource(vendure, { namespace: false });
+
+export default config({
+  graph: g,
+  auth: {
+    rules: (rules) => {
+      rules.public();
+    },
+  },
+});
